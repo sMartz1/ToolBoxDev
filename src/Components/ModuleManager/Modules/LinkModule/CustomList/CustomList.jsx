ee0f27dd41495b516ca2c77d0fab2d9ea5f238fc
Sebastian Martinez <ra3gan1991@gmail.com>
2023-03-24 17:02:29 +0100

    Styling and add new elements visuals

--- src/Components/ModuleManager/Modules/LinkModule/CustomList/CustomList.jsx
+++ src/Components/ModuleManager/Modules/LinkModule/CustomList/CustomList.jsx
@@ -1,29 +1,48 @@
 import React from "react";
 import CustomListItem from "./CustomListItem";
-import { Button, List } from "antd";
-import "./customList.scss"
+import { Button, List, Card } from "antd";
+import {
+    PlusOutlined,
+} from "@ant-design/icons";
+import "./customList.scss";
 
-    const ListRenderer = (props)=>{
+const addNewElement = (d) => {
+    let tempData = [...d];
+    tempData.push({ title: "new" });
+    return tempData;
+};
+
+const ListRenderer = (props) => {
     const { data } = props;
-    return  <List
-            size="small"
-            grid={{gutter: 9, column: 3}}
-            header={<h2>{data.title}</h2>}
-            bordered
-            dataSource={data.links}
-            rootClassName="list-link-container"
-            renderItem={(item) => <CustomListItem item={item} />}
-        />
-    
-}
+    if (data.title === "new") {
+        return (
+            <Card rootClassName="card-new-environment">
+                <div className="list-card-new-section">
+                    <Button type="primary" icon={<PlusOutlined />} />
+                </div>
+            </Card>
+        );
+    } else {
+        return (
+            <List
+                size="small"
+                grid={{ gutter: 9, column: 3 }}
+                header={<h2>{data.title}</h2>}
+                bordered
+                dataSource={addNewElement(data.links)}
+                rootClassName="list-link-container"
+                renderItem={(item) => <CustomListItem item={item} />}
+            />
+        );
+    }
+};
 
 export default function CustomList(props) {
     const { data } = props;
-   
 
     return (
         <>
-            {data.map((list) => (
+            {addNewElement(data).map((list) => (
                 <ListRenderer data={list} />
             ))}
         </>
--- src/Components/ModuleManager/Modules/LinkModule/CustomList/CustomListItem.jsx
+++ src/Components/ModuleManager/Modules/LinkModule/CustomList/CustomListItem.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 import { Button, List, Card } from "antd";
-export default function CustomListItem(props) {     
-        const { item } = props;
-
-    return <List.Item><Card title={item.title}>Card content</Card></List.Item>;
+import {
+    DeleteOutlined,
+    SettingOutlined,
+    UserOutlined,
+    ChromeOutlined,
+    PlusOutlined,
+} from "@ant-design/icons";
+export default function CustomListItem(props) {
+    const { item } = props;
+    const handleOpenSettings = () => {};
+    const cardExtras = (
+        <div className="card-options">
+            <Button
+                type="primary"
+                shape="circle"
+                icon={<SettingOutlined />}
+                size="small"
+                onClick={handleOpenSettings}
+                rootClassName="settingsButton"
+            />
+            <Button
+                type="primary"
+                shape="circle"
+                icon={<DeleteOutlined />}
+                size="small"
+                onClick={handleOpenSettings}
+                rootClassName="deleteButton"
+                danger
+            />
+        </div>
+    );
+    if (item.title === "new") {
+        return (
+            <List.Item>
+                <Card>
+                    <div className="list-card-new-container">
+                        <Button type="primary" icon={<PlusOutlined />} />
+                    </div>
+                </Card>
+            </List.Item>
+        );
+    } else {
+        return (
+            <List.Item>
+                <Card title={item.title} extra={cardExtras}>
+                    <div className="list-card-content-container">
+                        <Button type="primary" icon={<UserOutlined />}>
+                            Users
+                        </Button>
+                        <Button type="primary" icon={<ChromeOutlined />} />
+                    </div>
+                </Card>
+            </List.Item>
+        );
+    }
 }
